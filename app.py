971bd2569f3e5c22eae962c40ef334ac697a770b
LIN HUI <97448457+LinHui33@users.noreply.github.com>
2022-04-10 22:47:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from PIL import Image
 import torchvision.transforms as T
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-
+from keras.models import load_model
+from keras.preprocessing import image
 
 app = Flask(__name__)
 
@@ -56,5 +57,24 @@ def upload_file():
     else:
         return(render_template("main.html", result="pending"))
 
+@app.route('/CNN', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save("static/" + filename)
+        file = open("static/" + filename,"r")
+        model = load_model("CNN_ResNet50")
+        test_image = image.load_img("static/" + filename, target_size = (100, 100))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        pred = model.predict(test_image)
+        if pred[0][0] == 1:
+            s = "Covid"
+        else:
+            s = "Normal"
+        return(render_template("CNN.html", result=str(s)))
+    else:
+        return(render_template("CNN.html", result="pending"))
 if __name__ == "__main__":
     app.run()
